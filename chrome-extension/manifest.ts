ea7b5154fce1791cdc1f607c2d4f9222499c0f07
sanjay kumar <sanjuvanapatla55236@gmail.com>
2025-04-30 14:23:15 +0530

    Your message describing the changes

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -2,6 +2,21 @@ import { readFileSync } from 'node:fs';
 
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
 
+/**
+ * @prop default_locale
+ * if you want to support multiple languages, you can use the following reference
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
+ *
+ * @prop browser_specific_settings
+ * Must be unique to your extension to upload to addons.mozilla.org
+ * (you can delete if you only want a chrome extension)
+ *
+ * @prop permissions
+ * Firefox doesn't support sidePanel (It will be deleted in manifest parser)
+ *
+ * @prop content_scripts
+ * css: ['content.css'], // public folder
+ */
 const manifest = {
   manifest_version: 3,
   default_locale: 'en',
@@ -25,8 +40,9 @@ const manifest = {
     default_popup: 'popup/index.html',
     default_icon: 'icon-34.png',
   },
-  // chrome_url_overrides removed to allow Google homepage on new tab
-
+  chrome_url_overrides: {
+    newtab: 'new-tab/index.html',
+  },
   icons: {
     128: 'icon-128.png',
   },
